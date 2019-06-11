fixed alert message dialog and add feature variance and farmingham function

corr_explore/helpers.py
@@ -3,7 +3,8 @@ import pandas as pd
 from random import randint
 from services.naphyutils.pca import PcaUtil, LdaUtil
 from services.naphyutils.standardization import PreProcessingUtil
-ORDINAL = "ORDINAL"
+from .framingham import framingham_risk_score
+ORDINAL = "ORDINAL"  # not using
 NOMINAL = "NOMINAL"
 INTERVAL = "INTERVAL"
 
@@ -34,13 +35,13 @@ class Helper:
             number_type = '';
             nominal_values = []
             
-            if data_type in [np.int64, np.float64] and n_unique <= threshold_unique_ordinal:
-                # Set as ordinal number
-                number_type = ORDINAL
-                min = Helper.cast_number_for_json(data_type, col_series.min())
-                max = Helper.cast_number_for_json(data_type, col_series.max())
+#             if data_type in [np.int64, np.float64] and n_unique <= threshold_unique_ordinal:
+#                 # Set as ordinal number
+#                 number_type = ORDINAL
+#                 min = Helper.cast_number_for_json(data_type, col_series.min())
+#                 max = Helper.cast_number_for_json(data_type, col_series.max())
                 
-            elif data_type in [np.int64, np.float64] and n_unique > threshold_unique_ordinal:
+            if data_type in [np.int64, np.float64]:  # and n_unique > threshold_unique_ordinal:
                 # Set as interval number
                 number_type = INTERVAL
                 min = Helper.cast_number_for_json(data_type, col_series.min())
@@ -49,7 +50,7 @@ class Helper:
             elif data_type == 'object':
                 # Set as nominal
                 number_type = NOMINAL
-                nominal_values = list(unique_val)
+                nominal_values = list(np.sort(unique_val))
                 
             # Append object to array for result
             arr_columns.append({'column_name': col, 'number_type': number_type,
@@ -68,7 +69,7 @@ class Helper:
             return float(number)
 
     @staticmethod
-    def startify_mean(df_source, df_target, feature_indexes, arr_numtypes, arr_criterion, arr_bin, arr_groupby):
+    def startify_mean(df_source, df_target, arr_sel_source_col, arr_sel_target_col, arr_numtypes, arr_criterion, arr_bin, arr_groupby, is_calc_framingham):
         """
         Get number type and related criterion defind by user, group by to process data
         df_source: Dataframe for Source data such as radiomics data, it must be only number so that we can take mean values.
@@ -93,14 +94,23 @@ class Helper:
 #         len_target_col = df_target.shape[1] 
         
         # Select only the selected source columns 
-        arr_source_col_idx = feature_indexes.split(",")
-        int_source_col_idx = list(map(int, arr_source_col_idx))
+#         arr_source_col_idx = feature_indexes.split(",")
+        int_source_col_idx = list(map(int, arr_sel_source_col))
         df_selected_source = df_source.iloc[:, int_source_col_idx]
         arr_selected_source_col = list(df_selected_source.columns)
-        df_strat_res = df_selected_source.join(df_target)
-        arr_columns = list(df_target.columns)
         
-        df_strat_res = Helper.get_filtered_data(df_strat_res, arr_columns, arr_numtypes, arr_criterion)
+        int_target_col_idx = list(map(int, arr_sel_target_col))
+        df_selected_target = df_target.iloc[:, int_target_col_idx]
+        arr_selected_target_col = list(df_selected_target.columns)
+        
+        df_strat_res = df_selected_source.join(df_selected_target)
+        arr_columns = list(df_selected_target.columns)
+        
+        df_strat_res = Helper.get_filtered_data(df_strat_res, int_target_col_idx, arr_columns, arr_numtypes, arr_criterion)
+        
+        if df_strat_res.shape[0] == 0:
+            raise Exception("Cannot plot result due to no data remain after filtering by current selected condition.")
+        
         # Loop through all target columns to filter data
 #         for idx in range(0, len_target_col):
 #             
@@ -203,7 +213,8 @@ class Helper:
             arr_x_labels = []
             arr_y_vals = []
             arr_n_group_member = []
-#             cnt_group = 0
+            arr_fmh_avg_score = np.zeros(shape=(len(arr_selected_source_col), len_group))
+            
             # In case of categorical groupkeys = dict_keys(['Current', 'Never'])
             # 2 Level (Cate => Interval) ==> 
             for group_keys in groups.keys():
@@ -238,12 +249,17 @@ class Helper:
                     # Add number of member in group to display on chart
                     arr_n_group_member.append("#Member: " + str(groupped_data.shape[0]))
                     group_mean = groupped_data.mean()
-                
+                        
                     # Add mean value to each selected source column in radiomics
                     for s_idx in range(0, len(arr_selected_source_col)):
                         sel_col = arr_selected_source_col[s_idx]
                         group_mean_val = group_mean[sel_col]
                         arr_all_y_vals[s_idx][idx_arr_x] = group_mean_val
+                        
+                        # Add Framingham Risk Score
+                        if is_calc_framingham == True:
+                            avg_risk_percent, avg_score = framingham_risk_score(groupped_data)
+                            arr_fmh_avg_score[s_idx][idx_arr_x] = avg_risk_percent
                 
                 idx_arr_x = idx_arr_x + 1
             
@@ -255,6 +271,7 @@ class Helper:
             # change ndarray to list to prevent json problem
             trace['y_values'] = list(arr_all_y_vals[s_idx]) 
             trace['n_group_member'] = list(arr_n_group_member)
+            trace['framingham_risk_score'] = list(arr_fmh_avg_score[s_idx])
             arr_traces.append(trace)
         
         return arr_traces
@@ -288,13 +305,20 @@ class Helper:
         
         # Standardize data
         X_scaled = PreProcessingUtil.standardize(X)
-        dim_3d = []
-        if reduce_dim_algorithm == PCA:
-            # Get explain variance ratio
+        
+        # When 3 features are selected, skip doing PCA and directly return result from filtering and standard scalar.
+        if len_selected_source == 3:
+            return X_scaled, y
+        else:
+            dim_3d = []
             pca_helper = PcaUtil()
-            dim_3d = pca_helper.reduce_dimension(X_scaled, n_components=3)
-        elif reduce_dim_algorithm == LDA:
-            dim_3d = LdaUtil.reduce_dimension(X_scaled, y.values.ravel(), n_components=3)
+            if reduce_dim_algorithm == PCA:
+                # Get X transformed by PCA
+                dim_3d = pca_helper.reduce_dimension(X_scaled, n_components=3)
+                
+            elif reduce_dim_algorithm == LDA:
+                X_transformed = pca_helper.reduce_dimension(X_scaled, n_components=3)
+                dim_3d = LdaUtil.reduce_dimension(X_transformed, y.values.ravel(), n_components=3)
         
         return dim_3d, y
     
@@ -309,13 +333,15 @@ class Helper:
             return pd.DataFrame()
     
     @staticmethod
-    def get_filtered_data(df_data, arr_criterion_columns, arr_numtypes, arr_criterion):
+    def get_filtered_data(df_data, int_target_col_idx, arr_criterion_columns, arr_numtypes, arr_criterion):
         """
         Filter data by specifying conditions, type of number (range or specific)
         """
         # Loop through all target columns to filter data
-        len_target_col = len(arr_criterion_columns)
+        len_target_col = len(int_target_col_idx)
         for idx in range(0, len_target_col):
+            # Get index of row in table criterion because only select target column will be processed.
+            # idx = int_target_col_idx[iter_idx]
             col_name = arr_criterion_columns[idx]
             numtype = arr_numtypes[idx]  # INTERVAL, ORDINAL, NOMINAL
             # Cond vals are in format of cond1&cond2_val1,cond2_val2&cond3
@@ -325,8 +351,8 @@ class Helper:
             # For number value column "interval", process as range
             if numtype == INTERVAL or  numtype == ORDINAL:
                 cond_vals = str_cond_vals.split(",")
-                from_val = int(cond_vals[0])
-                to_val = int(cond_vals[1])
+                from_val = float(cond_vals[0])
+                to_val = float(cond_vals[1])
                 
                 df_data = df_data.loc[(df_data[col_name] >= from_val) & (df_data[col_name] <= to_val)]
                 
@@ -336,7 +362,7 @@ class Helper:
                 print(cond_vals)
                 df_data = df_data[df_data[col_name].isin(cond_vals)]
 
-            print("Shape result data: ", df_data.shape)
+            print("Shape result data: ", col_name, df_data.shape)
         return df_data
 
     @staticmethod

corr_explore/views.py
@@ -12,10 +12,15 @@ import pandas as pd
 from services.naphyutils.dataframe import DataFrameUtil
 from services.naphyutils.model import ModelUtils
 from services.naphyutils.feature_selection import FeatureSelectionUtil
+from services.naphyutils.biz_exception import BizValidationExption
 from . import views
 from .forms import FormUploadFile, FormStratifyData, DimensionReductionForm, FeatureSelectionForm
 from .helpers import Helper
 import constants.const_msg as msg
+
+from sklearn.preprocessing import MinMaxScaler
+
+import sys, traceback
 
 # Path under template folder
 
@@ -83,19 +88,47 @@ def stratify_data(request):
     # If form is valid, continue processing
     if form.is_valid():
          # Get parameter from form through cleaned_data to get correct data type
-
+      
+        target_strat = form.cleaned_data['target_strat']
         feature_indexes = form.cleaned_data['feature_indexes']
         numtypes = form.cleaned_data['numtypes']  # format: 'INTERVAL', 'NOMINAL', ...
         bins = form.cleaned_data['bin']  # format: 'INTERVAL', 'NOMINAL', ...
         criterion = form.cleaned_data['criterion']  # Format Male,Female&1938,1969&45,104&50,112 
-        groupby = form.cleaned_data['groupby']  # 
+        groupby = form.cleaned_data['groupby']  
+       # str_calc_framingham = form.cleaned_data['is_calc_framingham']
+        target_action = form.cleaned_data['target_action']
         
-        # Validate group by
+        is_calc_framingham = False
+        if target_action == "framingham":
+            is_calc_framingham = True
+        
+        arr_sel_target_col = target_strat.split(",")
+        arr_sel_source_col = feature_indexes.split(",")
         arr_groupby = groupby.split(",")
+        arr_numtype = numtypes.split(",")
+        arr_bin = bins.split(",")
+        
+        if all('' == s or s.isspace() for s in arr_sel_target_col):
+            form._errors["Include"] = form.error_class(["Target columns to group data must be selected."])
+            resp[msg.ERROR] = escape(form._errors)
+            return JsonResponse(resp)
+        
+        # Validate group by, if all group by values are space, show error
         if all('' == s or s.isspace() for s in arr_groupby):
             form._errors["Group By"] = form.error_class(["Group By must be entered."])
             resp[msg.ERROR] = escape(form._errors)
             return JsonResponse(resp)
+        
+        else:
+            # If group by is not empty and number is interval number, but Bin is empty.
+            # Show error
+            for row_idx in range(0, len(arr_groupby)):
+                if arr_groupby[row_idx] != ""  \
+                    and (arr_numtype[row_idx] == "INTERVAL" or arr_numtype[row_idx] == "ORDINAL") \
+                    and arr_bin[row_idx] == "":
+                    form._errors["Bin"] = form.error_class(["Number of Bin must be entered when group by an interval number."])
+                    resp[msg.ERROR] = escape(form._errors)
+                    return JsonResponse(resp)
         
         # print(feature_indexes, "/", criterion)
         df_source, df_target = get_source_target_dataframe(form)
@@ -108,16 +141,51 @@ def stratify_data(request):
         
         # Process data with the criterions
         arr_criterion = criterion.split("&")
-        arr_numtype = numtypes.split(",")
-        arr_bin = bins.split(",")
+
         arr_groupby = groupby.split(",")
-        arr_traces = Helper.startify_mean(df_source, df_target, feature_indexes, arr_numtype, \
-                                        arr_criterion, arr_bin, arr_groupby)
-        resp['traces'] = arr_traces
+        try:
+            
+            arr_traces = Helper.startify_mean(df_source, df_target, arr_sel_source_col, arr_sel_target_col, arr_numtype, \
+                                               arr_criterion, arr_bin, arr_groupby, is_calc_framingham)
+            
+            # If the target result is for plotting variance of stratified result
+            # continue to calculate variance bar
+            if target_action == "feature_variance":
+                arr_name, arr_value = get_strat_variance(arr_traces)
+                resp['feature_variances'] = {'features': arr_name, 'values': arr_value}
+                
+            resp['traces'] = arr_traces
+        except BizValidationExption as be:
+            label, err_msg = be.args
+            resp[msg.ERROR] = escape(format_html_err_msg(label, err_msg))
+        except Exception as e:
+           # Print error to console
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+#             print(repr(traceback.format_tb(exc_traceback)))
+#             print(traceback.format_exception(exc_type, exc_value,
+#                                           exc_traceback))
+            traceback.print_exc(limit=10, file=sys.stdout)
+            # Return error
+            err_msg = e.args
+            resp[msg.ERROR] = escape(format_html_err_msg("", err_msg[0]))
+                
     else:
         resp[msg.ERROR] = escape(form._errors)
         
     return JsonResponse(resp)
+
+
+def format_html_err_msg(label, msg):
+    if label != "" and msg != "":
+        return '<ul class="errorlist"><li>' + label \
+                +'<ul class="errorlist"><li>' \
+                +msg + "</li></ul></li></ul>"
+    else:
+        return '<ul class="errorlist"><li>' + msg + '</li></ul>'
+                # +'<ul class="errorlist"><li>' \
+               
+                # '</li></ul>'
+    # return '<strong>hey</strong>'
 
 
 def select_features(request):
@@ -155,6 +223,13 @@ def select_features(request):
 
 
 def reduce_dimension(request):
+    """
+        Filter data by all selected criterion.
+        In case of PCA is the target algorithm, the selected label will be used as for labelling only.
+        In case of PCA - LDA is selected, the label will be used as label for clustering predictors.
+        Result is return for 3D plot.
+        
+    """
     # Init response data
     resp = dict();
     
@@ -172,19 +247,40 @@ def reduce_dimension(request):
         criterion = form.cleaned_data['criterion']  
        
         df_source, df_target = get_source_target_dataframe(form);
-        
+
         # ======= Validation ===========
+        arr_feature_indexes = pca_feature_indexes.split(",");
+        n_selected_features = len(arr_feature_indexes)
+        if n_selected_features < 3:
+            form._errors["Number of selected features"] = form.error_class(["Number of selected features must be equal or greater than 3 for 3D space."])
+        
         # Validate both source and target row
         if df_source.shape[0] != df_target.shape[0] or not (df_target.shape[0] > 0): 
             form._errors["Number of row data"] = form.error_class(["Row data in files must be equal."])
-            resp[msg.ERROR] = escape(form._errors)
-            return JsonResponse(resp)
-        
+
         # Validate number of selected column if it's less than 3, error
-        if len(pca_feature_indexes.split(",")) < 3: 
-            form._errors["Number of features"] = form.error_class(["Number of selected features must be greater than 3."])
+#         if len(pca_feature_indexes.split(",")) < 3: 
+#             form._errors["Number of features"] = form.error_class(["Number of selected features must be greater than 3."])
+        
+        if not form.is_valid():
             resp[msg.ERROR] = escape(form._errors)
             return JsonResponse(resp)
+    
+#         # ======= Validation ===========
+#         if len(FormUploadFile.cleaned_data['pca_feature_indexes'].split(",")) < 3:
+#             form._errors["Number of selected features"] = form.error_class(["Number of selected features must be equal or greater than 3 for 3D space."])
+#         
+#         # Validate both source and target row
+#         if df_source.shape[0] != df_target.shape[0] or not (df_target.shape[0] > 0): 
+#             form._errors["Number of row data"] = form.error_class(["Row data in files must be equal."])
+#             resp[msg.ERROR] = escape(form._errors)
+#             return JsonResponse(resp)
+#         
+#         # Validate number of selected column if it's less than 3, error
+#         if len(pca_feature_indexes.split(",")) < 3: 
+#             form._errors["Number of features"] = form.error_class(["Number of selected features must be greater than 3."])
+#             resp[msg.ERROR] = escape(form._errors)
+#             return JsonResponse(resp)
         
         # Process request data
         arr_criterion = criterion.split("&")
@@ -231,3 +327,47 @@ def get_source_target_dataframe(form):
         df_target = DataFrameUtil.file_to_dataframe(target_file, header=target_column_header_idx)
          
     return  df_source, df_target
+
+
+def get_strat_variance(arr_traces):
+    """
+    arr_traces: Array of traces which is dict object with following key structure 
+            trace['trace_name'] => used
+            trace['x_labels']
+            trace['y_values']  => used
+            trace['n_group_member']
+            trace['framingham_risk_score']
+    Logic:
+        Data of each group (trace) is from stratified data for one radiomics feature.  
+            Scale all values from each data group to [0,1] scale 
+            Find variance of each group
+        When finish variance calculation for all groups,
+            Find percentile then sorted from max to min. 
+    """
+    
+    arr_temp_trace_name = []
+    arr_y_values = []
+    for trace in arr_traces:
+        arr_temp_trace_name.append(trace.get('trace_name'))
+        arr_y_values.append(trace.get('y_values'))
+        
+    # Default range is 0, 1
+    scaler = MinMaxScaler()
+    val_scaled = scaler.fit_transform(arr_y_values)
+    
+    # Find variance => var = mean(abs(x - x.mean())**2)
+    arr_variance = np.var(val_scaled, axis=1)
+    # min_max_index = np.argsort()
+    min_max_idx = np.argsort(arr_variance)
+    max_min_idx = min_max_idx[::-1]
+    # argsort(axis=0).argsort(axis=0)
+    # max_min_index = np.fliplr(min_max_index)
+    
+    # prepare result for x, y
+    arr_name = []
+    arr_value = []
+    for idx in max_min_idx:
+        arr_name.append(arr_temp_trace_name[idx])
+        arr_value.append(arr_variance[idx])
+    
+    return arr_name, arr_value