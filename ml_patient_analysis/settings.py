2584ab9628a6e7b1a19c60740b8404ba02218f51
Naphatthara <>
2019-07-01 15:52:21 +0200

    fixed bug for STATIC

--- ml_patient_analysis/settings.py
+++ ml_patient_analysis/settings.py
@@ -132,8 +132,10 @@ USE_TZ = True
 # custom
 import os.path
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_URL = '/static/'
