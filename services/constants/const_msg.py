ab93d324838635988b6ebcebac781e15936ff0bb
Naphatthara <>
2019-07-01 16:48:08 +0200

    delete dataset

--- services/constants/const_msg.py
+++
@@ -1,4 +0,0 @@
-SUCCESS = "msg_success"
-INFO = "msg_info"
-WARNING = "msg_warning"
-ERROR = "msg_error"
--- services/naphyutils/biz_exception.py
+++
@@ -1,9 +0,0 @@
-class BizValidationExption(Exception):
-
-    def __init__(self, label, value): 
-        self.label = label 
-        self.value = value 
-  
-    # __str__ is to print() the value 
-    def __str__(self): 
-        return(repr(self.label + self.value)) 
